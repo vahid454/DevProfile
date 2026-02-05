77b040d2a8edd4d10c2d154356853826503fcd4f
Vahid Mansuri <vahidmansuri702@gmail.com>
2026-02-05 16:56:21 +0530

    deployed web service to render

--- devprofile-frontend/src/app/services/portfolio.service.ts
+++ devprofile-frontend/src/app/services/portfolio.service.ts
@@ -8,7 +8,7 @@ import { catchError } from 'rxjs/operators';
 })
 export class PortfolioService {
   private apiUrl = (typeof window !== 'undefined' && window.location.hostname.endsWith('vercel.app'))
-    ? 'https://devprofile-yd3s.onrender.com/'
+    ? 'https://devprofile-yd3s.onrender.com'
     : '/api';
 
   constructor(private http: HttpClient) {}
